3ad01a4f2541dd359e4aa7a62fad4fb5bdaba5c8
Diego Garcia <noreply@localhost>
2025-11-25 01:06:22 -0500

    Add frametime graph and network usage to OSD overlay
    - Add frametime value and graph to detailed OSD level (3+)
    - Add network download/upload sensors using LibreHardwareMonitor
    - Add OSDItemNetwork showing download/upload speeds in KB/s or MB/s

--- Samples/XboxGamingBarHelper/RTSS/OSDItems/OSDItemFPS.cs
+++ Samples/XboxGamingBarHelper/RTSS/OSDItems/OSDItemFPS.cs
@@ -4,7 +4,17 @@
     {
         public override string GetOSDString(int osdLevel)
         {
-            return "<C=FF0000><APP><C> <C=FFFFFF><FR><S=50> FPS<S><C>";
+            // Basic: FPS only
+            var osdString = "<C=FF0000><APP><C> <C=FFFFFF><FR><S=50> FPS<S><C>";
+
+            // Detailed (level 3+): Add frametime graph
+            if (osdLevel >= 3)
+            {
+                // RTSS graph: width=60, height=20, min=0, max=50ms, data=frametime, flags
+                osdString += " <C=FFFF00><FT,1><S=50> ms<S><C> <G60,20,0,50,FT,0,0,30>";
+            }
+
+            return osdString;
         }
     }
 }
